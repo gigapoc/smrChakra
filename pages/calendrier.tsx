65489a0344fe4654af708fbfe2ecccd2e9d711a6
Lois <lois@vizuall.fr>
2021-04-29 17:39:31 +0200

    calendrier OK

---
+++ pages/calendrier.tsx
@@ -0,0 +1,32 @@
+import { Box, Container } from "@chakra-ui/layout"
+import FullCalendar from '@fullcalendar/react'
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import frLocale from '@fullcalendar/core/locales/fr';
+import "@fullcalendar/common/main.css"
+import "@fullcalendar/daygrid/main.css"
+
+interface Props {
+    
+}
+
+const Calendrier: React.FC<Props> = () => {
+
+    return <Box bg="main.100" minH="calc(100vh - 100px)">
+        <Container maxW="container.xl" mt={10} color='#fff'>
+            <FullCalendar 
+                plugins={[dayGridPlugin, googleCalendarPlugin]}
+                initialView='dayGridMonth'
+                height="80vh"
+                firstDay={1}
+                locale={frLocale}
+                googleCalendarApiKey="AIzaSyB1n1D3Yi3nGrZyeokjOTRYaaAMknjOHc8"
+                events={{
+                    googleCalendarId: "senartmultirotorracing@gmail.com"
+                }}
+            />
+        </Container>
+    </Box>
+}
+
+export default Calendrier;
