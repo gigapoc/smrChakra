72c131181e96978f5a7425c3c5d8bfc077e23942
Lois <lois@vizuall.fr>
2021-05-12 11:34:22 +0200

    better blog preview

--- src/components/blogPreview/index.tsx
+++ src/components/blogPreview/index.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Link, Text } from "@chakra-ui/layout"
+import { AspectRatio, Box, Link, Text, Flex} from "@chakra-ui/layout"
 import {Image} from "@chakra-ui/image"
 import { Article } from "pages/blog";
 import { Months } from "@components/blogArchives";
@@ -13,18 +13,18 @@ const BlogPreview: React.FC<Props> = ({article}) => {
     let dateArticle = new Date(article.dateArticle);
 
     let dateToDisplay = (dateArticle.getDate() < 10 ? "0":"") + dateArticle.getDate() + " " + Months[dateArticle.getMonth()] + ' ' + dateArticle.getFullYear()
-    console.log('ARTICLE', article.apercu)
+    // console.log('ARTICLE', article.apercu)
     return <Link href={"/article/" + article.id} _hover={{textDecoration: "none"}}>
-        <Box border="1px solid rgba(255,255,255,.2)" 
+        <Flex border="1px solid rgba(255,255,255,.2)" 
                     h="100%" 
                     borderRadius="2xl" 
                     maxW="450px" 
                     cursor="pointer" 
                     className='scaleOnHover'
                     bg="main.200"
-                    
+                    flexDir="column"
                     >
-            <AspectRatio maxW="100%" maxH="250px" ratio={16/9} className='aspectRatioContain'>
+            <AspectRatio maxW="100%" maxH="250px" ratio={16/9} className='aspectRatioContain' flexGrow={5}>
                 <Image src={process.env.NEXT_PUBLIC_URL_SERVER + article.apercu.url} 
                         alt={article.apercu.alternativeText}
                         objectFit="contain"
@@ -32,12 +32,14 @@ const BlogPreview: React.FC<Props> = ({article}) => {
                         borderRadius='15px 15px 15px 15px'
                         />
             </AspectRatio>
-            <Box h="80px" px='8' py="2">
-                <Text color="main.100" h="50px" fontSize="lg" fontWeight="bold" maxW="80%" overflow="hidden">{article.title}</Text>
-                <Text color="main.100" mt="2px" className="blogViews"><FaRegEye/> {article.views}</Text>
-                <Text color="main.100" h="20px" fontSize="md" w="150px" overflow="hidden" float='right' textAlign="right" opacity={.8}>{dateToDisplay}</Text>
-            </Box>
-        </Box>
+            <Flex px='8' py="2" flexGrow={4} flexDir="column" flexBasis={"80px"}>
+                <Box color="main.100" h="50px" fontSize="lg" fontWeight="bold" maxW="80%" overflow="hidden" flexGrow={5}>{article.title}</Box>
+                <Box flexGrow={1}>
+                    <Box color="main.100" mt="2px" className="blogViews"><FaRegEye/> {article.views}</Box>
+                    <Box color="main.100" h="20px" fontSize="md" w="150px" overflow="hidden" float='right' textAlign="right" opacity={.8}>{dateToDisplay}</Box>
+                </Box>
+            </Flex>
+        </Flex>
     </Link>
 }
 
