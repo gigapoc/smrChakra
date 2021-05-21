d7569ded4ffcc7009129a0eb6bb27ab838ed9f3c
Lois <lois@vizuall.fr>
2021-05-21 12:13:38 +0200

    smaller chronologie

--- pages/chronologie.tsx
+++ pages/chronologie.tsx
@@ -11,8 +11,8 @@ const Chronologie : React.FC<Props> = ({}) => {
         title="Chronologie SMR"
         description="Toutes les dates clés du Sénart Multirotor Racing !"
         />
-        <Box bg="main.100" minH="calc(100vh - 100px)" color="main.200">
-            <Container maxW="container.xl" h="calc(100vh - 100px)">
+        <Box bg="main.100" minH="calc(100vh - 100px)" color="main.200" pt={"5vh"}>
+            <Container maxW="container.xl" h="calc(90vh - 100px)">
                 <iframe className="iFrameChrono"
                         src="https://lucid.app/documents/embeddedchart/71a510e6-7423-4383-a5bb-b558cf320141"/>
                         
