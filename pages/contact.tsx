0ae913a48dd406c0d3e7969df49db28c7f752565
Lois <lois@vizuall.fr>
2021-06-16 11:28:22 +0200

    better contact form mobile ,

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -117,6 +117,7 @@ const Contact: React.FC<Props> = ({}) => {
                 </VStack>
             </Popup>
         </Box>
+        <Box display={{sm: 'block', md: "none"}} h="100px"/>
     </>
 }
 
