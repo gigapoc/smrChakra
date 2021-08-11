e1c3e7d98f2aa9fd4385272889e51d54c0894cca
Lois <lois@vizuall.fr>
2021-08-11 16:05:29 +0200

    mobile opt

--- src/components/embedVideo/index.tsx
+++ src/components/embedVideo/index.tsx
@@ -1,7 +1,10 @@
 import { AspectRatio, Box, ResponsiveValue } from '@chakra-ui/react'
 import React from 'react'
 import {
-    isBrowser
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
   } from "react-device-detect";
 
 interface Props {
@@ -12,9 +15,17 @@ export const EmbedVideo: React.FC<Props> = ({height}) => {
 
     return <Box h={height} width={"100%"} mt={1}>
         <AspectRatio maxH={height} >
-            <video controls autoPlay muted loop height={height} >
-                <source src={"https://assets.senart-multirotor-racing.com/" + (isBrowser ? "raider.mp4":"output_star_wars.webm")} type="video/webm"/>
-            </video>
+            <BrowserView>
+                <video controls autoPlay muted loop height={height} >
+                    <source src={"https://assets.senart-multirotor-racing.com/raider.mp4"} type="video/mp4"/>
+                </video>
+            </BrowserView>
+            <MobileView>
+                <video controls autoPlay muted height={height} >
+                    <source src={"https://assets.senart-multirotor-racing.com/output_star_wars.webm"} type="video/webm"/>
+                </video>
+            </MobileView>
+            
             {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/qtZ9NxpUN2o?controls=0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
         </AspectRatio>
     </Box>
