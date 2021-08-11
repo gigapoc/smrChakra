fb439a7d7b4c9083c241416c923b1c11015470dc
Lois <lois@vizuall.fr>
2021-08-11 15:59:57 +0200

    better perf for mobile ?

--- src/components/embedVideo/index.tsx
+++ src/components/embedVideo/index.tsx
@@ -1,5 +1,8 @@
 import { AspectRatio, Box, ResponsiveValue } from '@chakra-ui/react'
 import React from 'react'
+import {
+    isBrowser
+  } from "react-device-detect";
 
 interface Props {
     height: string | ResponsiveValue<any>;
@@ -10,7 +13,7 @@ export const EmbedVideo: React.FC<Props> = ({height}) => {
     return <Box h={height} width={"100%"} mt={1}>
         <AspectRatio maxH={height} >
             <video controls autoPlay muted loop height={height} >
-                <source src="https://assets.senart-multirotor-racing.com/raider.mp4" type="video/webm"/>
+                <source src={"https://assets.senart-multirotor-racing.com/" + (isBrowser ? "raider.mp4":"output_star_wars.webm")} type="video/webm"/>
             </video>
             {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/qtZ9NxpUN2o?controls=0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
         </AspectRatio>
